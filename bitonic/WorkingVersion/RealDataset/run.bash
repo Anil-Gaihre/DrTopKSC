66dd0902b109f0010723fb182903303849f7873f
Anil-Gaihre <35698558+Anil-Gaihre@users.noreply.github.com>
2021-06-04 15:38:20 -0400

    Update run.bash

--- bitonic/WorkingVersion/RealDataset/run.bash
+++ bitonic/WorkingVersion/RealDataset/run.bash
@@ -4,44 +4,23 @@ alpha=5
 N=30
 #k=33554431
 beta=2
-declare -a arr=("UK_DrBitonic.csv" "kmer_V1r_DrBitonic.csv" "KNN_DrBitonic.csv" "UK_log" "kmer_V1r_log" "KNN_log" "U_K64" "U_K128" "U_K256" "U_K512")
+declare -a arr=("ClueWeb09_degree.csv" "kmer_V1r_DrBitonic.csv" "KNN_DrBitonic.csv" "ClueWeb09_degree_log" "kmer_V1r_log" "KNN_log" "U_K64" "U_K128" "U_K256" "U_K512")
 #UK: 105153953: N =26
 #kmer_V1r : V=214005017: N= 27
 
 #declare -a k=(1 2 4 8 16 31 64 128 256 512)
 #for (( beta=2; beta<3; beta=beta+1 ))
-N=26
+N=30
 for (( k=1; k<33554431; k=k*2 ))
 do
-	for (( alpha=10;alpha<=16; alpha=alpha+1 ))
+	for (( alpha=5;alpha<=16; alpha=alpha+1 ))
 	do
-		./topk.bin $N $k $alpha /scratch/datasets/topkData/graph_project_start/graph_reader/degreeUK.dat ${arr[0]} >> ${arr[3]}
+		./topk.bin $N $k $alpha /scratch/datasets/topkData/graph_project_start/graph_reader/ClueWeb09_degree.dat ${arr[0]} >> ${arr[3]}
 		#./topk.bin  29 33554431 8 2
 		echo -n "Finihed processing for a k "
 	done
 done
 printf "\n"
 
-N=27
-for (( k=1; k<33554431; k=k*2 ))
-do
-	for (( alpha=10;alpha<=16; alpha=alpha+1 ))
-	do
-		./topk.bin $N $k $alpha /scratch/datasets/topkData/graph_project_start/graph_reader/degreeKmer.dat ${arr[1]} >> ${arr[4]}
-		#./topk.bin  29 33554431 8 2
-		echo -n "Finihed processing for a k "
-	done
-done
-printf "\n"
 
-# for (( k=1; k<33554431; k=k*2 ))
-# do
-# 	for (( N=26; N<=26; N=N+1 ))
-# 	do
-# 		./topk.bin $N $k $alpha /scratch/datasets/topkData/graph_project_start/graph_reader/KNN.dat ${arr[2]} >> ${arr[5]}
-# 		#./topk.bin  29 33554431 8 2
-# 		echo -n "Finihed processing for a k "
-# 	done
-# done
-# printf "\n"
 
